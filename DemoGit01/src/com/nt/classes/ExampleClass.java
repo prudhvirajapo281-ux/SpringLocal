ccd152b9d743cba6b2510676f4ce19da17a594c9
prudviraj <prudhvirajapo281@gmail.com>
2025-11-16 17:04:02 +0530

    tl commit

---
+++ DemoGit01/src/com/nt/classes/ExampleClass.java
@@ -0,0 +1,12 @@
+package com.nt.classes;
+
+public class ExampleClass {
+	int a=10;
+	int b=20;
+	
+	public void show()
+	{
+		System.out.println(a+b);
+	}
+	
+}
---
+++ DemoGit01/src/com/nt/classes/Main.java
@@ -0,0 +1,9 @@
+package com.nt.classes;
+
+public class Main {
+  public static void main(String[] args) {
+	
+	  ExampleClass ex=new ExampleClass();
+	  ex.show();
+}
+}
