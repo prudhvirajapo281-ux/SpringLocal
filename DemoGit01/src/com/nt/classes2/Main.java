6e15c83d8fe771356fb38f8ab271428dba08f1e0
prudviraj <prudhvirajapo281@gmail.com>
2025-11-16 17:18:06 +0530

    classs2 added

---
+++ DemoGit01/src/com/nt/classes2/Main.java
@@ -0,0 +1,9 @@
+package com.nt.classes2;
+
+public class Main {
+  public static void main(String[] args) {
+	sum1 s=new sum1();
+	s.sub();
+	s.sum();
+}
+}
---
+++ DemoGit01/src/com/nt/classes2/sum1.java
@@ -0,0 +1,15 @@
+package com.nt.classes2;
+
+public class sum1 {
+  int a=10;
+  int b=20;
+  void sum()
+  {
+	  System.out.println(a+b);
+  }
+  
+  void sub()
+  {
+	  System.out.println(a-b);
+  }
+}
